5f6bcbd2c75d387ca1e0a0bf0f8a332f96580e80
karf <karf@karf.cz>
2013-04-20 00:31:44 +0200

    Added tests for kff.List

--- test/kff.List.test.js
+++ test/kff.List.test.js
@@ -49,7 +49,59 @@ describe('kff.List', function()
 		list.insert('B', 1);
 
 		list.count.should.eql(3);
-		list.findByIndex(1).should.equal('B');
+		list.get(1).should.equal('B');
+	});
+
+	it('should get value from the list', function()
+	{
+		var list = new kff.List();
+		list.append('A');
+		list.get(0).should.equal('A');
+	});
+
+	it('should set value in the list', function()
+	{
+		var list = new kff.List();
+		list.append('A');
+		list.set(0, 'B');
+		list.get(0).should.equal('B');
+		list.count.should.equal(1);
+	});
+
+	it('should not set value on nonexistent index in the list', function(done)
+	{
+		var list = new kff.List();
+		try
+		{
+			list.set(42, 'B');
+		}
+		catch (error)
+		{
+			list.count.should.equal(0);
+			done();
+		}
+	});
+
+	it('should empty the list', function()
+	{
+		var list = new kff.List();
+		list.append('A');
+		list.empty();
+		list.count.should.equal(0);
+	});
+
+	it('should iterate for each item in the list', function()
+	{
+		var list = new kff.List();
+		var count = 0;
+		list.append('A');
+		list.append('B');
+		list.each(function(item, i){
+			count++;
+			if(i === 0) item.should.equal('A');
+			if(i === 1) item.should.equal('B');
+		});
+		count.should.equal(2);
 	});
 
 });
