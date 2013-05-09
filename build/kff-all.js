3f84b8d86a9c04c0f02f4255d3859b09774483a9
karf <karf@karf.cz>
2013-05-09 13:09:47 +0200

    A new class kff.App encapsulates most common minimal application structure (service container, front controller, deafult view, view factory).

--- build/kff-all.js
+++ build/kff-all.js
@@ -3332,5 +3332,66 @@ kff.FrontController = kff.createClass(
 });
 
 
+kff.App = kff.createClass(
+/** @lends kff.App.prototype */
+{
+	/**
+		@constructs
+	*/
+	constructor: function(options)
+	{
+		var models;
+		options = options || {};
+		models = options.models || {};
+
+		// Dependency injection container configuration:
+		var config = {
+			parameters: {},
+			services: {
+				viewFactory: {
+					construct: 'kff.ViewFactory',
+					args: [{
+						serviceContainer: '@'
+					}],
+					shared: true
+				},
+				frontController: {
+					construct: 'kff.FrontController',
+				    args: [{
+				    	viewFactory: '@viewFactory',
+				    	defaultView: 'pageView'
+				    }],
+				    shared: true
+				},
+				pageView: {
+					construct: 'kff.PageView',
+					args: [{
+				    	viewFactory: '@viewFactory',
+				    	models: models
+				    }]
+				}
+			}
+		};
+
+		this.serviceContainer = new kff.ServiceContainer(config);
+
+		if('services' in options) this.serviceContainer.registerServices(options.services, true);
+
+		return this;
+	},
+
+	init: function()
+	{
+		this.serviceContainer.getService('frontController').init();
+	},
+
+	getServiceContainer: function()
+	{
+		return this.serviceContainer;
+	}
+
+});
+
+
 
 })(this);
