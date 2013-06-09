3971723aab8790763149063365e6ddf54dbe91c8
karf <karf@karf.cz>
2013-06-09 12:54:33 +0200

    Removed TemplateBinder due to the conflicts with rendering of subviews.

--- build/kff-all.js
+++ build/kff-all.js
@@ -3162,38 +3162,6 @@ kff.TextBinder = kff.createClass(
 kff.BindingView.registerBinder('text', kff.TextBinder);
 
 
-kff.TemplateBinder = kff.createClass(
-{
-	extend: kff.Binder
-},
-/** @lends kff.TemplateBinder.prototype */
-{
-	/**
-	 * One-way data binder for html content of the element.
-	 * Renders html content of the element on change of the bound model attribute.
-	 * Inserted content will be processed for eventual subviews and bindings
-	 *
-	 * @constructs
-	 * @augments kff.Binder
-	 * @param {Object} options Options object
-	 */
-	constructor: function(options)
-	{
-		kff.Binder.call(this, options);
-	},
-
-	refresh: function()
-	{
-		this.view.destroySubviews();
-		this.$element.html(this.values.join(' '));
-		this.view.renderSubviews();
-
-	}
-});
-
-kff.BindingView.registerBinder('template', kff.TemplateBinder);
-
-
 kff.ValueBinder = kff.createClass(
 {
 	extend: kff.Binder
