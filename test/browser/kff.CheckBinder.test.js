267885611963066da414c0437b2d9acfeb458240
karf <karf@karf.cz>
2014-02-12 23:36:42 +0100

    test(kff.CheckBinder): update test according to the new behavior
    Check binder now reacts only to the click event.

--- test/browser/kff.CheckBinder.test.js
+++ test/browser/kff.CheckBinder.test.js
@@ -2,7 +2,7 @@ if(typeof require === 'function') var kff = require('../build/kff-all.js');
 
 describe('kff.CheckBinder', function()
 {
-	it('should bind check binder', function(done)
+	it('should bind check binder', function()
 	{
 		var $input = $('<input type="checkbox" data-kff-bind="myModel.checked:check"/>');
 		var view = new kff.BindingView(
@@ -16,19 +16,11 @@ describe('kff.CheckBinder', function()
 		});
 		view.init();
 
-		setTimeout(function()
-		{
-			$input.is(':checked').should.equal(true);
-			view.getModel('myModel').set('checked', false);
-			$input.is(':checked').should.equal(false);
-			$input.prop('checked', true).trigger('change');
-			setTimeout(function()
-			{
-				view.getModel('myModel').get('checked').should.equal(true);
-				done();
-			}, 0);
-		}, 0);
-
+		$input.is(':checked').should.equal(true);
+		view.getModel('myModel').set('checked', false);
+		$input.is(':checked').should.equal(false);
+		$input.prop('checked', true).triggerHandler('click');
+		view.getModel('myModel').get('checked').should.equal(true);
 	});
 
 });
