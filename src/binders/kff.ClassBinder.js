2719d5e00e33ce8490106e7176ce7c4f691a9d5b
karf <karf@karf.cz>
2014-10-18 11:38:36 +0200

    feat(kff.ClassNotBinder): create a new binder 'classnot' that works like the 'class' binder with the default 'negate' parameter turned on

--- src/binders/kff.ClassBinder.js
+++ src/binders/kff.ClassBinder.js
@@ -1,79 +1,89 @@
-
-kff.ClassBinder = kff.createClass(
+var createClassBinder = function(negate)
 {
-	extend: kff.Binder
-},
-/** @lends kff.ClassBinder.prototype */
-{
-	/**
-	 * One-way data binder (model to DOM) for CSS class.
-	 * Sets/Unsets the class of the element to some predefined value when model atrribute changes.
-	 *
-	 * @constructs
-	 * @augments kff.Binder
-	 * @param {Object} options Options objectt
-	 */
-	constructor: function(options)
+	var ClassBinder = kff.createClass(
 	{
-		kff.Binder.call(this, options);
+		extend: kff.Binder
 	},
-
-	init: function()
+	/** @lends kff.ClassBinder.prototype */
 	{
-		this.className = this.options.params[0] || null;
-		this.equalsTo = true;
-
-		if(this.options.params[1])
+		/**
+		 * One-way data binder (model to DOM) for CSS class.
+		 * Sets/Unsets the class of the element to some predefined value when model atrribute changes.
+		 *
+		 * @constructs
+		 * @augments kff.Binder
+		 * @param {Object} options Options objectt
+		 */
+		constructor: function(options)
 		{
-			if(this.options.parsers.length === 0) this.equalsTo = this.convertValueType(this.options.params[1]);
-			else this.equalsTo = this.parse(this.options.params[1]);
-			if(this.equalsTo == null) this.equalsTo = null;
-		}
+			kff.Binder.call(this, options);
+		},
 
-		this.operator = this.options.params[2] || null;
-		kff.ClassBinder._super.init.call(this);
-	},
-
-	refresh: function()
-	{
-		if(this.className)
+		init: function()
 		{
-			if(this.matchValue())
+			this.className = this.options.params[0] || null;
+			this.equalsTo = true;
+
+			if(this.options.params[1])
 			{
-				this.$element[0].classList.add(this.className);
+				if(this.options.parsers.length === 0) this.equalsTo = this.convertValueType(this.options.params[1]);
+				else this.equalsTo = this.parse(this.options.params[1]);
+				if(this.equalsTo == null) this.equalsTo = null;
 			}
-			else
-			{
-				this.$element[0].classList.remove(this.className);
-			}
-		}
-	},
 
-	matchValue: function()
-	{
-		if(this.options.params.length > 1)
-		{
-			var value = this.value;
-			if(value == null) value = null;
-			if(this.operator === 'ne') return value !== this.equalsTo;
-			else return value === this.equalsTo;
-		}
-		else return this.value;
-	}
-});
+			this.negate = this.options.params[2] === 'ne' || negate;
 
-if(typeof document === 'object' && document !== null)
-{
-	if(!('classList' in document.documentElement))
-	{
-		kff.ClassBinder.prototype.refresh = function(value)
+			kff.ClassBinder._super.init.call(this);
+		},
+
+		refresh: function()
 		{
 			if(this.className)
 			{
-				this.$element[this.matchValue() ? 'addClass' : 'removeClass'](this.className);
+				if(this.matchValue())
+				{
+					this.$element[0].classList.add(this.className);
+				}
+				else
+				{
+					this.$element[0].classList.remove(this.className);
+				}
 			}
-		};
+		},
+
+		matchValue: function()
+		{
+			if(this.options.params.length > 1)
+			{
+				var value = this.value;
+				if(value == null) value = null;
+				if(this.negate) return value !== this.equalsTo;
+				else return value === this.equalsTo;
+			}
+			else return this.value;
+		}
+	});
+
+	if(typeof document === 'object' && document !== null)
+	{
+		if(!('classList' in document.documentElement))
+		{
+			ClassBinder.prototype.refresh = function(value)
+			{
+				if(this.className)
+				{
+					this.$element[this.matchValue() ? 'addClass' : 'removeClass'](this.className);
+				}
+			};
+		}
 	}
-}
+
+	return ClassBinder;
+
+};
+
+kff.ClassBinder = createClassBinder(false);
+kff.ClassNotBinder = createClassBinder(true);
 
 kff.BindingView.registerBinder('class', kff.ClassBinder);
+kff.BindingView.registerBinder('classnot', kff.ClassNotBinder);
