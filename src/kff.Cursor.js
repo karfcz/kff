e62eb6142b4321725a8d6e36b5c25b455775fe54
karf <karf@karf.cz>
2015-02-05 11:38:20 +0100

    feat(kff.Cursor): add kff.Cursor file

---
+++ src/kff.Cursor.js
@@ -0,0 +1,54 @@
+
+kff.Cursor = kff.createClass(
+{
+	constructor: function(keyPath, root)
+	{
+		this.root = root;
+		this.keyPath = keyPath || [];
+	},
+
+	refine: function(keyPath)
+	{
+		return new kff.Cursor(this.keyPath.concat(keyPath), this.root);
+	},
+
+	get: function()
+	{
+		return kff.evalObjectPath(this.keyPath, this.root);
+	},
+
+	// getIn: function(keyPath)
+	// {
+	// 	return kff.evalObjectPath(this.keyPath.concat(keyPath), this.root);
+	// },
+
+	set: function(value)
+	{
+		if(this.keyPath.length < 1) return;
+		var prop = this.keyPath[0];
+		var keyPath = this.keyPath.slice(1);
+		this.root[prop] = kff.imset(keyPath, value, this.root[prop]);
+	},
+
+	// setIn: function(value, keyPath)
+	// {
+	// 	this.refine(keyPath).set(value);
+	// },
+
+	update: function(fn)
+	{
+		if(this.keyPath.length < 1) return;
+		var prop = this.keyPath[0];
+		var keyPath = this.keyPath.slice(1);
+		this.root[prop] = kff.imset(keyPath, fn, this.root[prop]);
+	},
+
+	remove: function()
+	{
+		if(this.keyPath.length < 1) return;
+		var prop = this.keyPath[0];
+		var keyPath = this.keyPath.slice(1);
+		this.root[prop] = kff.imremove(keyPath, this.root[prop]);
+	}
+
+});
