9fd7310cfad92247e0b7282eb53bf160934155d0
karf <karf@karf.cz>
2015-02-24 08:57:32 +0100

    refactor(kff.View): reuse data-kff-bind attribute fetched during tree walk phase in binding init phase

--- src/kff.View.js
+++ src/kff.View.js
@@ -127,6 +127,12 @@ kff.View = kff.createClass(
 			this.env = options.env;
 		}
 
+		if(options.bindAttr)
+		{
+			this.bindAttr = options.bindAttr;
+		}
+		else this.bindAttr = null;
+
 		this.options = options;
 
 	},
@@ -343,6 +349,7 @@ kff.View = kff.createClass(
 					options = subviewsStruct[i].options;
 					options.element = subviewsStruct[i].$element[0];
 					options.env = this.env;
+					options.bindAttr = subviewsStruct[i].bindAttr;
 					subView = this.createView(subviewsStruct[i].viewName, options);
 					if(subView instanceof kff.View)
 					{
@@ -632,7 +639,7 @@ kff.View = kff.createClass(
 	 */
 	findViewElements: function(el)
 	{
-		var node = el, viewName = null, rendered, onAttr, optAttr, index = 0, subviewsStruct = null;
+		var node = el, viewName = null, rendered, onAttr, optAttr, index = 0, subviewsStruct = null, bindAttr;
 
 		while((node = this.nextNode(el, node, viewName === null)) !== null)
 		{
@@ -642,7 +649,8 @@ kff.View = kff.createClass(
 			if(!rendered)
 			{
 				viewName = node.getAttribute(kff.DATA_VIEW_ATTR);
-				if(!viewName && node.getAttribute(kff.DATA_BIND_ATTR))
+				bindAttr = node.getAttribute(kff.DATA_BIND_ATTR);
+				if(!viewName && bindAttr)
 				{
 					viewName = 'kff.View';
 					node.setAttribute(kff.DATA_VIEW_ATTR, viewName);
@@ -655,7 +663,8 @@ kff.View = kff.createClass(
 						viewName: viewName,
 						index: index,
 						$element: $(node),
-						options: optAttr ? JSON.parse(optAttr) : {}
+						options: optAttr ? JSON.parse(optAttr) : {},
+						bindAttr: bindAttr
 					});
 				}
 			}
@@ -1071,8 +1080,10 @@ kff.View = kff.createClass(
 	initBinding: function()
 	{
 		var model, attr, result, result2, modelPathArray, i, ret, modelArgs;
-		var dataBindAttr = this.$element[0].getAttribute(kff.DATA_BIND_ATTR);
+		var dataBindAttr = this.bindAttr;
 		var modelName;
+
+		if(!dataBindAttr) return;
 
 		bindingRegex.lastIndex = 0;
 
