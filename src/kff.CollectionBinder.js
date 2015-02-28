8127f5117a77c9b99de8971a79d83b2465aa135d
karf <karf@karf.cz>
2015-02-28 19:26:18 +0100

    fix(kff.CollectionBinder): do not try to generate bound views in fast path when parentNode is null

--- src/kff.CollectionBinder.js
+++ src/kff.CollectionBinder.js
@@ -184,19 +184,19 @@ kff.CollectionBinder = kff.createClass(
 				if(this.anchor.parentNode)
 				{
 					parentNode = this.anchor.parentNode;
-				}
-				for(i = 0; i < l; i++)
-				{
-					boundView = this.createBoundView(a[i]);
-					el = boundView.$element[0];
-					parentNode.insertBefore(el, lastChild.nextSibling);
-					boundView.setBindingIndex(i);
-					lastChild = el;
-				}
+					for(i = 0; i < l; i++)
+					{
+						boundView = this.createBoundView(a[i]);
+						el = boundView.$element[0];
+						parentNode.insertBefore(el, lastChild.nextSibling);
+						boundView.setBindingIndex(i);
+						lastChild = el;
+					}
 
-				for(i = 0; i < l; i++)
-				{
-					this.boundViews[i].runAll();
+					for(i = 0; i < l; i++)
+					{
+						this.boundViews[i].runAll();
+					}
 				}
 			}
 		}
