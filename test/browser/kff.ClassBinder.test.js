0fa035fa95858fe81773135dd1b4da37913d9371
karf <karf@karf.cz>
2015-09-15 10:35:37 +0200

    test: add tests for ClassBinder

---
+++ test/browser/kff.ClassBinder.test.js
@@ -0,0 +1,84 @@
+if(typeof require === 'function') var kff = require('../build/kff.js');
+
+describe('kff.ClassBinder', function()
+{
+	it('should set a class through class binder', function()
+	{
+		var myModel = new kff.Cursor({
+			name: 'Karel'
+		});
+
+		var $div = $('<div data-kff-bind="myModel.name:class(myClass, Petr)"/>');
+		var view = new kff.View(
+		{
+			element: $div,
+			scope: {
+				myModel: myModel
+			}
+		});
+		view.renderAll();
+		view.runAll();
+
+		expect($div[0].classList.contains('myClass')).to.equal(false);
+
+		myModel.setIn('name', 'Petr');
+
+		view.refreshAll();
+
+		expect($div[0].classList.contains('myClass')).to.equal(true);
+	});
+
+	it('should set a class through class binder using dynamic scope value lookup', function()
+	{
+		var myModel = new kff.Cursor({
+			name: 'Karel',
+			value: 'Petr'
+		});
+
+		var $div = $('<div data-kff-bind="myModel.name:class(myClass, @myModel.value)"/>');
+		var view = new kff.View(
+		{
+			element: $div,
+			scope: {
+				myModel: myModel
+			}
+		});
+		view.renderAll();
+		view.runAll();
+
+		expect($div[0].classList.contains('myClass')).to.equal(false);
+
+		myModel.setIn('name', 'Petr');
+
+		view.refreshAll();
+
+		expect($div[0].classList.contains('myClass')).to.equal(true);
+	});
+
+	it('should set a class through classnot binder', function()
+	{
+		var myModel = new kff.Cursor({
+			name: 'Karel'
+		});
+
+		var $div = $('<div data-kff-bind="myModel.name:classnot(myClass, Petr)"/>');
+		var view = new kff.View(
+		{
+			element: $div,
+			scope: {
+				myModel: myModel
+			}
+		});
+		view.renderAll();
+		view.runAll();
+
+		expect($div[0].classList.contains('myClass')).to.equal(true);
+
+		myModel.setIn('name', 'Petr');
+
+		view.refreshAll();
+
+		expect($div[0].classList.contains('myClass')).to.equal(false);
+	});
+
+});
