fca8366493a5d32a7f5fea43d0fb0a42c6476993
karfcz <karf@karf.cz>
2016-05-30 14:57:46 +0200

    fix(deepFreeze): check if argument is object

--- src/functions/deepFreeze.js
+++ src/functions/deepFreeze.js
@@ -1,21 +1,19 @@
 
 function deepFreeze(o)
 {
-	try {
-	if(!Object.isFrozen(o)) Object.freeze(o);
-
-	} catch(e) {
-		console.log(o);
-		throw(e);
-	}
-
-	Object.getOwnPropertyNames(o).forEach(function (prop)
+	if(typeof o === 'object' && o !== null)
 	{
-		if(o.hasOwnProperty(prop) && o[prop] !== null && typeof o[prop] === "object" && !Object.isFrozen(o[prop]))
+		if(!Object.isFrozen(o)) Object.freeze(o);
+
+		Object.getOwnPropertyNames(o).forEach(function (prop)
 		{
-			deepFreeze(o[prop]);
-		}
-	});
+			if(o.hasOwnProperty(prop) && o[prop] !== null && typeof o[prop] === "object" && !Object.isFrozen(o[prop]))
+			{
+				deepFreeze(o[prop]);
+			}
+		});
+
+	}
 
 	return o;
 };
