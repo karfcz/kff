1298f8cca1c46c0aac651a887b91332e93a1ec53
karf <karf@karf.cz>
2016-07-15 11:48:30 +0200

    feat(DisabledNotBinder): implement first parameter in the same way as in the DisabledBinder + test

--- src/binders/DisabledNotBinder.js
+++ src/binders/DisabledNotBinder.js
@@ -2,6 +2,7 @@
 var createClass = require('../functions/createClass');
 var Binder = require('../Binder');
 var View = require('../View');
+var Cursor = require('../Cursor');
 
 var DisabledNotBinder = createClass(
 {
@@ -20,16 +21,54 @@ var DisabledNotBinder = createClass(
 	constructor: function(options)
 	{
 		Binder.call(this, options);
+		this.equalsTo = undefined;
+		this.valueCursor = undefined;
+	},
+
+	init: function()
+	{
+		this.equalsTo = true;
+		if(this.options.params[0])
+		{
+			this.equalsTo = this.convertBindingValue(this.options.params[0]);
+			if(this.equalsTo instanceof Cursor)
+			{
+				this.valueCursor = this.equalsTo;
+				this.equalsTo = this.valueCursor.get();
+			}
+			if(this.equalsTo == null) this.equalsTo = null;
+		}
+		DisabledNotBinder._super.init.call(this);
+	},
+
+	matchValue: function()
+	{
+		var value = this.value;
+		if(value == null) value = null;
+		this.updateEqualsToValue();
+		return value !== this.equalsTo;
+	},
+
+	updateEqualsToValue: function()
+	{
+		if(this.options.params.length > 0)
+		{
+			if(this.valueCursor)
+			{
+				this.equalsTo = this.valueCursor.get();
+				if(this.equalsTo == null) this.equalsTo = null;
+			}
+		}
 	},
 
 	refresh: function()
 	{
-		this.element.disabled = !this.value;
+		this.element.disabled = this.matchValue();
 	},
 
 	fill: function()
 	{
-		if(!this.fillVal) this.fillVal = !this.element.disabled;
+		if(!this.fillVal) this.fillVal = this.element.disabled ? false : this.equalsTo;
 		this.updateModel(this.fillVal);
 	}
 });
---
+++ test/browser/DisabledNotBinder.test.js
@@ -0,0 +1,55 @@
+if(typeof require === 'function') var kff = require('../build/kff.js');
+
+describe('kff.DisabledNotBinder', function()
+{
+	it('should bind disablednot binder', function()
+	{
+		var state = new kff.Cursor(false);
+
+		var input = document.createElement('input');
+		input.setAttribute('type', 'text');
+		input.setAttribute('data-kff-bind', 'state:disablednot');
+
+		var view = new kff.View(
+		{
+			element: input,
+			scope: {
+				state: state
+			}
+		});
+		view.initAll();
+
+		expect(input.disabled).to.equal(true);
+		state.set(true);
+		view.refreshAll();
+		expect(input.disabled).to.equal(false);
+
+	});
+
+
+	it('should bind disablednot binder to non-boolean state', function()
+	{
+		var disabledValue = 'disabledValue';
+		var state = new kff.Cursor(disabledValue);
+
+		var input = document.createElement('input');
+		input.setAttribute('type', 'text');
+		input.setAttribute('data-kff-bind', 'state:disablednot(disabledValue)');
+
+		var view = new kff.View(
+		{
+			element: input,
+			scope: {
+				state: state
+			},
+		});
+		view.initAll();
+
+		expect(input.disabled).to.equal(false);
+		state.set('undisabledValue');
+		view.refreshAll();
+
+		expect(input.disabled).to.equal(true);
+	});
+
+});
