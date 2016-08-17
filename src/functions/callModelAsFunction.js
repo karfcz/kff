d29c544981caea931afcbdeec0e0de5c44bb0765
karf <karf@karf.cz>
2016-08-17 15:48:32 +0200

    refactor: complete rewrite of binding parser engine (WIP)

--- src/functions/callModelAsFunction.js
+++ src/functions/callModelAsFunction.js
@@ -1,17 +1,6 @@
 
-var convertValueType = require('./convertValueType');
-
 function callModelAsFunction(view, model, modelArgs)
 {
-	// var args = [];
-	// if(Array.isArray(modelArgs))
-	// {
-	// 	for(var i = 0, l = modelArgs.length; i < l; i++)
-	// 	{
-	// 		if(Array.isArray(modelArgs[i])) args[i] = view.getCursor(modelArgs[i]);
-	// 		else args[i] = convertValueType(modelArgs[i]);
-	// 	}
-	// }
 	return model.apply(null, modelArgs || []);
 }
 
